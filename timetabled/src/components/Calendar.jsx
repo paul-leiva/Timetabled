d41c70c9eb6b33c12b40e1bf4d6dfb920ec2c777
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2024-09-17 20:23:22 -0400

    Step 2

---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            Testing the calendar component
+        </div>
+    )
+}
+
+export default Calendar;
